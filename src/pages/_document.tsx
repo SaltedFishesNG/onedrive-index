b72bcb4d16413b96150ea07e6da2fce6919e2b8d
SaltedFishes <main@saltedfishes.com>
2026-04-06 17:22:31 +0000

    Remove unused resources

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,12 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="description" content="OneDrive Index" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+          <link rel="apple-touch-icon" type="image/png" href="/apple-touch-icon.png" sizes="180x180" />
+          <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192" />
+          <link rel="icon" type="image/png" href="/android-chrome-512x512.png" sizes="512x512" />
         </Head>
         <body>
           <Main />
